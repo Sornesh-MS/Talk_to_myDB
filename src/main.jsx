d09f8e5c05af46380c95c0a7be046ec5d44c46bd
Sornesh-MS <sorneshms@gmail.com>
2026-02-01 14:53:46 +0530

    for forntend

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TalkToMyDB from './app.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TalkToMyDB />
+  </React.StrictMode>,
+)
